cf7a32f4adcdb1070490c24f4b69e7591f36fc18
Aldis Berjoza <aldis@bsdroot.lv>
2010-08-06 10:13:09 +0300

    Fix typo in license

--- bin/playd.sh
+++ bin/playd.sh
@@ -3,7 +3,7 @@
 # License {{{1
 
 # Copyright (c) 2009-2010, Aldis Berjoza <aldis@bsdroot.lv>
-# All rights reserverd.
+# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
